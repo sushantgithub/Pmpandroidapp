009a72e00697dd10c9116b2006d944f716caf104
sushantgithub <kulk.sushant@gmail.com>
2026-09-27 00:13:41 +0530

    Bump CompMastery to v1.1 detailed course

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sushant.compmastery"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
